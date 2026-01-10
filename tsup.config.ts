e8b74499ad8b959659fe5057b114fd91eb9eace0
Léo Mathurin <leo.mathurin@epitech.eu>
2026-01-10 18:46:58 +0100

    trying ruchernchong tsup approach

---
+++ tsup.config.ts
@@ -0,0 +1,24 @@
+import { defineConfig } from "tsup";
+
+export default defineConfig({
+  entry: ["src/index.ts"],
+  format: ["esm"],
+  target: "node20",
+  outDir: "dist",
+  clean: true,
+  sourcemap: true,
+  minify: false,
+  bundle: true,
+  splitting: false,
+  treeshake: true,
+  dts: false,
+  banner: {
+    js: `import { createRequire } from 'module';
+import { fileURLToPath } from 'url';
+import { dirname } from 'path';
+
+const require = createRequire(import.meta.url);
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);`,
+  },
+});
